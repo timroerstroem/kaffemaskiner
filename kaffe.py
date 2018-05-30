7b831a005a8207b14b4b6a6f43a31d76a2d7d9c3
Tim Rørstrøm <timroerstroem@gmail.com>
2018-05-30 13:49:29 +0200

    Wrote loop to simulate queue

--- kaffe.py
+++ kaffe.py
@@ -12,10 +12,40 @@ Assumptions:
 """
 
 import statistics as stat
+import random
 
 
+# The number of ticks to run the simulation (3 h * 60 min/h * 4 ticks/min)
+stop = 3 * 60 * 4
 # The queue length at each time interval (start at 0 for time 0)
 queue = [0]
 people = 96  # number of people on each floor
 # How many cups of coffee does the average person drink in the morning?
-cups = 4
+cups = 3
+# Probability of a person getting a cup of coffee at any given tick
+pCoffee = cups / stop
+timeToCoffee = 0
+t = 1
+
+
+while t < stop:
+    if timeToCoffee > 0:
+        timeToCoffee -= 1
+
+    if queue[t-1] > 0 and timeToCoffee == 0:
+        # Someone was waiting in line and the coffee machine is free
+        queue[t] = queue[t-1] - 1
+        timeToCoffee = 3
+
+    if len(queue) == t:
+        queue[t] = queue[t-1]
+
+    # See if anyone wants coffee
+    if random.random() < people * pCoffee:
+        # One person has decided to get coffee, check if the machine is free
+        if queue[t] == 0 and timeToCoffee == 0:
+            timeToCoffee = 3
+        else:
+            queue[t] += 1
+
+    t += 1
